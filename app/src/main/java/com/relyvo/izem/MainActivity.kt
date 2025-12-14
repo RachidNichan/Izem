3739f99776c4c5a99c3598d037cf20025195a979
Rachid Nichan <rachidarn95@gmail.com>
2025-12-14 20:00:10 +0100

    Add Splash Screen using Android 12 API

--- app/src/main/java/com/relyvo/izem/MainActivity.kt
+++ app/src/main/java/com/relyvo/izem/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.relyvo.izem
 
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.relyvo.izem.data.DataSource // تأكد من استدعاء ملف البيانات
-import com.relyvo.izem.ui.theme.IzemTheme // قد يكون الاسم مختلفاً قليلاً حسب اسم مشروعك
+import com.relyvo.izem.data.DataSource
+import com.relyvo.izem.ui.theme.IzemTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        var keepSplashOnScreen = true
+        splashScreen.setKeepOnScreenCondition { keepSplashOnScreen }
+        lifecycleScope.launch {
+            delay(1500)
+            keepSplashOnScreen = false
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             IzemTheme {
